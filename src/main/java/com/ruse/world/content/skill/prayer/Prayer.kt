ab5fd1fed4185caefd3774e79d69bfac69770553
Titas <titas@lacunaclientscope.com>
2022-10-04 21:35:51 +0100

    refactoring java to kotlin prayer

--- src/main/java/com/ruse/world/content/skill/prayer/Prayer.kt
+++ src/main/java/com/ruse/world/content/skill/prayer/Prayer.kt
@@ -1,58 +1,56 @@
-package com.ruse.world.content.skill.prayer;
+package com.ruse.world.content.skill.prayer
 
-import com.ruse.engine.task.Task;
-import com.ruse.engine.task.TaskManager;
-import com.ruse.model.Animation;
-import com.ruse.model.Item;
-import com.ruse.model.Skill;
-import com.ruse.world.content.Achievements;
-import com.ruse.world.content.Achievements.AchievementData;
-import com.ruse.world.content.Sounds;
-import com.ruse.world.content.Sounds.Sound;
-import com.ruse.model.entity.character.player.Player;
+import com.ruse.engine.task.Task
+import com.ruse.engine.task.TaskManager
+import com.ruse.model.Animation
+import com.ruse.model.Skill
+import com.ruse.world.content.Achievements
+import com.ruse.world.content.Achievements.AchievementData
+import com.ruse.model.Item
+import com.ruse.model.entity.character.player.Player
+import com.ruse.world.content.Sounds
 
 /**
  * The prayer skill is based upon burying the corpses of enemies. Obtaining a higher level means
  * more prayer abilities being unlocked, which help out in combat.
- * 
+ *
  * @author Gabriel Hannason
  */
+object Prayer {
+    @JvmStatic
+    fun isBone(bone: Int): Boolean {
+        return BonesData.forId(bone) != null
+    }
 
-public class Prayer {
-	
-	public static boolean isBone(int bone) {
-		return BonesData.forId(bone) != null;
-	}
-	
-	public static void buryBone(final Player player, final int itemId) {
-		if(!player.getClickDelay().elapsed(2000))
-			return;
-		final BonesData currentBone = BonesData.forId(itemId);
-		if(currentBone == null)
-			return;
-		player.getSkillManager().stopSkilling();
-		player.getPacketSender().sendInterfaceRemoval();
-		player.performAnimation(new Animation(827));
-		player.getPacketSender().sendMessage("You dig a hole in the ground..");
-		final Item bone = new Item(itemId);
-		player.getInventory().delete(bone);
-		TaskManager.submit(new Task(3, player, false) {
-			@Override
-			public void execute() {
-				player.getPacketSender().sendMessage("..and bury the "+bone.getDefinition().getName()+".");
-				player.getSkillManager().addExperience(Skill.PRAYER, currentBone.getBuryingXP());
-				Sounds.sendSound(player, Sound.BURY_BONE);
-				if(currentBone == BonesData.BIG_BONES)
-					Achievements.finishAchievement(player, AchievementData.BURY_A_BIG_BONE);
-				if(currentBone == BonesData.DRAGON_BONES)
-					Achievements.finishAchievement(player, AchievementData.BURY_A_DRAGON_BONE);
-				else if(currentBone == BonesData.FROSTDRAGON_BONES) {
-					Achievements.doProgress(player, AchievementData.BURY_25_FROST_DRAGON_BONES);
-					Achievements.doProgress(player, AchievementData.BURY_500_FROST_DRAGON_BONES);
-				}
-				stop();				
-			}
-		});
-		player.getClickDelay().reset();
-	}
+    @JvmStatic
+    fun buryBone(player: Player, itemId: Int) {
+        if (!player.clickDelay.elapsed(2000)) return
+        val currentBone = BonesData.forId(itemId) ?: return
+        player.skillManager.stopSkilling()
+        player.packetSender.sendInterfaceRemoval()
+        player.performAnimation(Animation(827))
+        player.packetSender.sendMessage("You dig a hole in the ground..")
+        val bone = Item(itemId)
+        player.inventory.delete(bone)
+        TaskManager.submit(object : Task(3, player, false) {
+            public override fun execute() {
+                player.packetSender.sendMessage("..and bury the " + bone.definition.name + ".")
+                player.skillManager.addExperience(Skill.PRAYER, currentBone.buryingXP)
+                Sounds.sendSound(player, Sounds.Sound.BURY_BONE)
+                if (currentBone == BonesData.BIG_BONES) Achievements.finishAchievement(
+                    player,
+                    AchievementData.BURY_A_BIG_BONE
+                )
+                if (currentBone == BonesData.DRAGON_BONES) Achievements.finishAchievement(
+                    player,
+                    AchievementData.BURY_A_DRAGON_BONE
+                ) else if (currentBone == BonesData.FROSTDRAGON_BONES) {
+                    Achievements.doProgress(player, AchievementData.BURY_25_FROST_DRAGON_BONES)
+                    Achievements.doProgress(player, AchievementData.BURY_500_FROST_DRAGON_BONES)
+                }
+                stop()
+            }
+        })
+        player.clickDelay.reset()
+    }
 }
