69627c6af8532e1e83664da17a9faef04076c43c
Titas <titas@lacunaclientscope.com>
2022-10-05 16:38:31 +0100

    refactoring to kotlin CombatSession.kt

--- src/main/java/com/realting/world/content/combat/CombatSession.kt
+++ src/main/java/com/realting/world/content/combat/CombatSession.kt
@@ -1,146 +1,133 @@
-package com.realting.world.content.combat;
+package com.realting.world.content.combat
 
-import com.realting.engine.task.Task;
-import com.realting.model.container.impl.Equipment;
-import com.realting.model.entity.character.npc.NPC;
-import com.realting.model.entity.character.player.Player;
-import com.realting.world.content.Sounds;
-import com.realting.world.content.combat.HitQueue.CombatHit;
-import com.realting.world.content.combat.range.CombatRangedAmmo.RangedWeaponData;
-import com.realting.world.content.combat.strategy.impl.DefaultRangedCombatStrategy;
-import com.realting.world.content.combat.weapon.CombatSpecial;
+import com.realting.model.container.impl.Equipment
+import com.realting.model.entity.character.npc.NPC
+import com.realting.model.entity.character.player.Player
+import com.realting.world.content.Sounds
+import com.realting.world.content.combat.HitQueue.CombatHit
+import com.realting.world.content.combat.range.CombatRangedAmmo.RangedWeaponData
+import com.realting.world.content.combat.strategy.impl.DefaultRangedCombatStrategy
+import com.realting.world.content.combat.weapon.CombatSpecial
 
 /**
- * A {@link Task} implementation that handles every combat 'hook' or 'turn'
+ * A [Task] implementation that handles every combat 'hook' or 'turn'
  * during a combat session.
- * 
+ *
  * @author lare96
  */
-public class CombatSession {
+class CombatSession
+/**
+ * Create a new [CombatSession].
+ *
+ * @param builder
+ * the builder assigned to this task.
+ */(
+    /** The builder assigned to this task.  */
+    private val builder: CombatBuilder
+) {
+    fun process() {
+        if (builder.isCooldown) {
+            builder.cooldown--
+            builder.attackTimer--
+            if (builder.cooldown == 0) {
+                builder.reset(true)
+            }
+            return
+        }
+        if (!CombatFactory.checkHook(builder.character, builder.victim)) {
+            return
+        }
 
-	/** The builder assigned to this task. */
-	private CombatBuilder builder;
+        // If the entity is an player we redetermine the combat strategy before
+        // attacking.
+        if (builder.character.isPlayer) {
+            builder.determineStrategy()
+        }
 
-	/**
-	 * Create a new {@link CombatSession}.
-	 * 
-	 * @param builder
-	 * the builder assigned to this task.
-	 */
-	public CombatSession(CombatBuilder builder) {
-		this.builder = builder;
-	}
-	
-	public void process() {
+        // Decrement the attack timer.
+        builder.attackTimer--
 
-		if (builder.isCooldown()) {
-			builder.cooldown--;
-			builder.attackTimer--;
+        // The attack timer is below 1, we can attack.
+        if (builder.attackTimer < 1) {
+            // Check if the attacker is close enough to attack.
+            if (!CombatFactory.checkAttackDistance(builder)) {
+                if (builder.character.isNpc && builder.victim.isPlayer) {
+                    if (builder.lastAttack.elapsed(4500)) {
+                        (builder.character as NPC).setFindNewTarget(true)
+                    }
+                }
+                return
+            }
 
-			if (builder.cooldown == 0) {
-				builder.reset(true);
-			}
-			return;
-		}
+            // Check if the attack can be made on this hook
+            if (!builder.strategy.canAttack(builder.character, builder.victim)) {
+                builder.character.combatBuilder.reset(if (builder.character.isNpc) true else false)
+                return
+            }
 
-		if (!CombatFactory.checkHook(builder.getCharacter(), builder.getVictim())) {
-			return;
-		}
+            // Do all combat calculations here, we create the combat containers
+            // using the attacking entity's combat strategy.
+            builder.strategy.customContainerAttack(builder.character, builder.victim)
+            var container = builder.container
+            builder.character.setEntityInteraction(builder.victim)
+            if (builder.character.isPlayer) {
+                val player = builder.character as Player
+                player.packetSender.sendInterfaceRemoval()
+                if (player.isSpecialActivated && player.castSpell == null) {
+                    container = player.combatSpecial.container(player, builder.victim)
+                    val magicShortbowSpec =
+                        player.combatSpecial != null && player.combatSpecial === CombatSpecial.MAGIC_SHORTBOW
+                    CombatSpecial.drain(player, player.combatSpecial.drainAmount)
+                    Sounds.sendSound(
+                        player, Sounds.specialSounds(
+                            player.equipment[Equipment.WEAPON_SLOT].id
+                        )
+                    )
+                    if (player.combatSpecial.combatType === CombatType.RANGED) {
+                        DefaultRangedCombatStrategy.decrementAmmo(player, builder.victim.position)
+                        if (CombatFactory.darkBow(player) || player.rangedWeaponData == RangedWeaponData.MAGIC_SHORTBOW && magicShortbowSpec) {
+                            DefaultRangedCombatStrategy.decrementAmmo(player, builder.victim.position)
+                        }
+                    }
+                }
+            }
 
-		// If the entity is an player we redetermine the combat strategy before
-		// attacking.
-		if (builder.getCharacter().isPlayer()) {
-			builder.determineStrategy();
-		}
+            // If there is no hit type the combat turn is ignored.
+            if (container != null && container.combatType != null) {
+                // If we have hit splats to deal, we filter them through combat
+                // prayer effects now. If not then we still send the hit tasks
+                // next to handle any effects.
 
-		// Decrement the attack timer.
-		builder.attackTimer--;
+                // An attack is going to be made for sure, set the last attacker
+                // for this victim.
+                builder.victim.combatBuilder.lastAttacker = builder.character
+                builder.victim.lastCombat.reset()
 
-		// The attack timer is below 1, we can attack.
-		if (builder.attackTimer < 1) {
-			// Check if the attacker is close enough to attack.
-			if (!CombatFactory.checkAttackDistance(builder)) {
-				if(builder.getCharacter().isNpc() && builder.getVictim().isPlayer()) {
-					if(builder.getLastAttack().elapsed(4500)) {
-						((NPC)builder.getCharacter()).setFindNewTarget(true);
-					}
-				}
-				return;
-			}
-
-			// Check if the attack can be made on this hook
-			if (!builder.getStrategy().canAttack(builder.getCharacter(), builder.getVictim())) {
-				builder.getCharacter().getCombatBuilder().reset(builder.getCharacter().isNpc() ? true : false);
-				return;
-			}
-
-			// Do all combat calculations here, we create the combat containers
-			// using the attacking entity's combat strategy.
-
-			builder.getStrategy().customContainerAttack(builder.getCharacter(), builder.getVictim());
-			CombatContainer container = builder.getContainer();
-
-			builder.getCharacter().setEntityInteraction(builder.getVictim());
-
-			if (builder.getCharacter().isPlayer()) {
-				Player player = (Player) builder.getCharacter();
-				player.getPacketSender().sendInterfaceRemoval();
-
-				if (player.isSpecialActivated() && player.getCastSpell() == null) {
-					container = player.getCombatSpecial().container(player, builder.getVictim());
-					boolean magicShortbowSpec = player.getCombatSpecial() != null && player.getCombatSpecial() == CombatSpecial.MAGIC_SHORTBOW;
-					CombatSpecial.drain(player, player.getCombatSpecial().getDrainAmount());
-
-					Sounds.sendSound(player, Sounds.specialSounds(player.getEquipment().get(Equipment.WEAPON_SLOT).getId()));
-
-					if (player.getCombatSpecial().getCombatType() == CombatType.RANGED) {
-						DefaultRangedCombatStrategy.decrementAmmo(player, builder.getVictim().getPosition());
-						if(CombatFactory.darkBow(player) || player.getRangedWeaponData() == RangedWeaponData.MAGIC_SHORTBOW && magicShortbowSpec) {
-							DefaultRangedCombatStrategy.decrementAmmo(player, builder.getVictim().getPosition());
-						}
-					}
-				}
-			}
-
-			// If there is no hit type the combat turn is ignored.
-			if (container != null && container.getCombatType() != null) {
-				// If we have hit splats to deal, we filter them through combat
-				// prayer effects now. If not then we still send the hit tasks
-				// next to handle any effects.
-
-				// An attack is going to be made for sure, set the last attacker
-				// for this victim.
-				builder.getVictim().getCombatBuilder().setLastAttacker(builder.getCharacter());
-				builder.getVictim().getLastCombat().reset();
-
-				// Start cooldown if we're using magic and not autocasting.
-				if (container.getCombatType() == CombatType.MAGIC && builder.getCharacter().isPlayer()) {
-					Player player = (Player) builder.getCharacter();
-
-					if (!player.isAutocast()) {
-						if (!player.isSpecialActivated())
-							player.getCombatBuilder().cooldown = 10;
-						player.setCastSpell(null);
-						player.getMovementQueue().setFollowCharacter(null);
-						builder.determineStrategy();
-					}
-				}
+                // Start cooldown if we're using magic and not autocasting.
+                if (container.combatType === CombatType.MAGIC && builder.character.isPlayer) {
+                    val player = builder.character as Player
+                    if (!player.isAutocast) {
+                        if (!player.isSpecialActivated) player.combatBuilder.cooldown = 10
+                        player.castSpell = null
+                        player.movementQueue.followCharacter = null
+                        builder.determineStrategy()
+                    }
+                }
 
 //				if(container.getHitDelay() == 0) { //An instant attack
 //					new CombatHitTask(builder, container).handleAttack();
 //				} else {
 //					TaskManager.submit(new CombatHitTask(builder, container, container.getHitDelay(), false));
 //				}
+                builder.hitQueue.append(CombatHit(builder, container, container.hitDelay))
+                builder.container = null //Fetch a brand new container on next attack
+            }
 
-				builder.getHitQueue().append(new CombatHit(builder, container, container.getHitDelay()));
-
-				builder.setContainer(null); //Fetch a brand new container on next attack
-			}
-
-			// Reset the attacking entity.
-			builder.attackTimer = builder.getStrategy() != null ? builder.getStrategy().attackDelay(builder.getCharacter()) : builder.getCharacter().getAttackSpeed();
-			builder.getLastAttack().reset();
-			builder.getCharacter().setEntityInteraction(builder.getVictim());
-		}
-	}
+            // Reset the attacking entity.
+            builder.attackTimer =
+                if (builder.strategy != null) builder.strategy.attackDelay(builder.character) else builder.character.attackSpeed
+            builder.lastAttack.reset()
+            builder.character.setEntityInteraction(builder.victim)
+        }
+    }
 }
