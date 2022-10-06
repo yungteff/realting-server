b991a10e78cd07622bd230e8ad081dac0c6b0692
Titas <titas@lacunaclientscope.com>
2022-10-06 10:26:58 +0100

    refactor to kotlin EntityHandler.kt

--- src/main/java/com/realting/model/entity/EntityHandler.kt
+++ src/main/java/com/realting/model/entity/EntityHandler.kt
@@ -1,47 +1,55 @@
-package com.realting.model.entity;
+package com.realting.model.entity
 
-import com.realting.engine.task.TaskManager;
-import com.realting.model.GameObject;
-import com.realting.net.PlayerSession;
-import com.realting.net.SessionState;
-import com.realting.world.World;
-import com.realting.world.clip.region.RegionClipping;
-import com.realting.world.content.CustomObjects;
-import com.realting.model.entity.character.npc.NPC;
-import com.realting.model.entity.character.player.Player;
+import com.realting.engine.task.TaskManager
+import com.realting.model.GameObject
+import com.realting.model.entity.character.npc.NPC
+import com.realting.model.entity.character.player.Player
+import com.realting.net.SessionState
+import com.realting.world.World
+import com.realting.world.clip.region.RegionClipping
+import com.realting.world.content.CustomObjects
 
-public class EntityHandler {
+object EntityHandler {
+    @JvmStatic
+    fun register(entity: Entity) {
+        when {
+            entity.isPlayer -> {
+                val player = entity as Player
+                val session = player.session
+                if (session.state == SessionState.LOGGING_IN && !World.getLoginQueue().contains(player)) {
+                    World.getLoginQueue().add(player)
+                }
+            }
+            entity.isNpc -> {
+                val npc = entity as NPC
+                World.getNpcs().add(npc)
+            }
+            entity.isGameObject -> {
+                val gameObject = entity as GameObject
+                RegionClipping.addObject(gameObject)
+                CustomObjects.spawnGlobalObjectWithinDistance(gameObject)
+            }
+        }
+    }
 
-	public static void register(Entity entity) {
-		if(entity.isPlayer()) {
-			Player player = (Player) entity;
-			PlayerSession session = player.getSession();
-			if (session.getState() == SessionState.LOGGING_IN && !World.getLoginQueue().contains(player)) {
-				World.getLoginQueue().add(player);
-			}
-		} if(entity.isNpc()) {
-			NPC npc = (NPC) entity;
-			World.getNpcs().add(npc);
-		} else if(entity.isGameObject()) {
-			GameObject gameObject = (GameObject) entity;
-			RegionClipping.addObject(gameObject);
-			CustomObjects.spawnGlobalObjectWithinDistance(gameObject);
-		}
-	}
-
-	public static void deregister(Entity entity) {
-		if(entity.isPlayer()) {
-			Player player = (Player)entity;
-			World.getPlayers().remove(player);
-		} else if(entity.isNpc()) {
-			NPC npc = (NPC)entity;
-			TaskManager.cancelTasks(npc.getCombatBuilder());
-			TaskManager.cancelTasks(entity);
-			World.getNpcs().remove(npc);
-		} else if(entity.isGameObject()) {
-			GameObject gameObject = (GameObject) entity;
-			RegionClipping.removeObject(gameObject);
-			CustomObjects.deleteGlobalObjectWithinDistance(gameObject);
-		}
-	}
+    @JvmStatic
+    fun deregister(entity: Entity) {
+        when {
+            entity.isPlayer -> {
+                val player = entity as Player
+                World.getPlayers().remove(player)
+            }
+            entity.isNpc -> {
+                val npc = entity as NPC
+                TaskManager.cancelTasks(npc.combatBuilder)
+                TaskManager.cancelTasks(entity)
+                World.getNpcs().remove(npc)
+            }
+            entity.isGameObject -> {
+                val gameObject = entity as GameObject
+                RegionClipping.removeObject(gameObject)
+                CustomObjects.deleteGlobalObjectWithinDistance(gameObject)
+            }
+        }
+    }
 }
