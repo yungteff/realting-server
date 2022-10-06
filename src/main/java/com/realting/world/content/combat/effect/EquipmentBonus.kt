b4e670b82d09354f074f0abd01528bf71137f460
Titas <titas@lacunaclientscope.com>
2022-10-06 06:53:25 +0100

    refactoring to kotlin EquipmentBonus.kt

--- src/main/java/com/realting/world/content/combat/effect/EquipmentBonus.kt
+++ src/main/java/com/realting/world/content/combat/effect/EquipmentBonus.kt
@@ -1,49 +1,38 @@
-package com.realting.world.content.combat.effect;
+package com.realting.world.content.combat.effect
 
-import com.realting.model.container.impl.Equipment;
-import com.realting.model.entity.character.npc.NPC;
-import com.realting.model.entity.character.player.Player;
+import com.realting.model.container.impl.Equipment
+import com.realting.model.entity.character.npc.NPC
+import com.realting.model.entity.character.player.Player
 
-public class EquipmentBonus {
-	
-	public static int[] helms = {11665, 11664, 11663}; //11665 == melee, 11664 == range, 11663 == mage
-	public static int gloves = 8842;
-	public static int[] normRobes = {8839, 8840};
-	public static int[] eRobes = {19785, 19786};
-	public static int deflector = 19712;
-	public static int mace = 8841;
-	public static final int[] obsidianMeleeWeapons = {
-			6523, 6528, 6527, 6525
-		};
+object EquipmentBonus {
+    var helms = intArrayOf(11665, 11664, 11663) //11665 == melee, 11664 == range, 11663 == mage
+    var gloves = 8842
+    var normRobes = intArrayOf(8839, 8840)
+    var eRobes = intArrayOf(19785, 19786)
+    var deflector = 19712
+    var mace = 8841
+    val obsidianMeleeWeapons = intArrayOf(
+        6523, 6528, 6527, 6525
+    )
 
-		public static boolean berserkerNecklaceEffect(Player player) {		
-			boolean good = false;
-			if (player.getEquipment().get(Equipment.AMULET_SLOT).getId() == 11128) {
-			
-				for (int i = 0; i < obsidianMeleeWeapons.length; i++) {
-					if (player.getEquipment().get(Equipment.WEAPON_SLOT).getId() == obsidianMeleeWeapons[i]) {
-						good = true;
-						break;
-					}
-				}
-				
-			}
-			
-			if (good) {
-				return true;
-			}
-			return false;
-		}
+    fun berserkerNecklaceEffect(player: Player): Boolean {
+        var good = false
+        if (player.equipment[Equipment.AMULET_SLOT].id == 11128) {
+            for (i in obsidianMeleeWeapons.indices) {
+                if (player.equipment[Equipment.WEAPON_SLOT].id == obsidianMeleeWeapons[i]) {
+                    good = true
+                    break
+                }
+            }
+        }
+        return good
+    }
 
-	
-	public static boolean slayerMageBonus(Player player, NPC npc) {
-		if(player.getSlayer().isSlayerTask(npc) && (player.getEquipment().getItems()[Equipment.HEAD_SLOT].getId() == 15492 || player.getEquipment().getItems()[Equipment.HEAD_SLOT].getId() == 15488)) {
-			return true;
-		}
-		return false;
-	}
+    fun slayerMageBonus(player: Player, npc: NPC?): Boolean {
+        return player.slayer.isSlayerTask(npc) && (player.equipment.items[Equipment.HEAD_SLOT].id == 15492 || player.equipment.items[Equipment.HEAD_SLOT].id == 15488)
+    }
 
-	/*
+    /*
 		HELMS -> 11665 Melee, 11664 Range, 11663 Mage
 		DEFLECTOR -> 19712
 		BODY -> 8839
@@ -52,64 +41,57 @@ public class EquipmentBonus {
 		ELITE BODY -> 19785
 		ELITE LEGS -> 19786
 	 */
-	
-	public static boolean voidElite(Player player){ 
-		if (wearingVoid(player) && (player.checkItem(Equipment.BODY_SLOT, eRobes[0]) && player.checkItem(Equipment.LEG_SLOT, eRobes[1]))) {
-			return true;
-		}
-		return false;
-	}
-	
-	public static boolean voidRange(Player player){
-		if (wearingVoid(player) && player.checkItem(Equipment.HEAD_SLOT, helms[1])) {
-			return true;
-		}
-		return false;
-	}
-	
-	public static boolean voidMelee(Player player){
-		if (wearingVoid(player) && player.checkItem(Equipment.HEAD_SLOT, helms[0])) {
-			return true;
-		}
-		return false;
-	}
-	
-	public static boolean voidMage(Player player){
-		if (wearingVoid(player) && player.checkItem(Equipment.HEAD_SLOT, helms[2])) {
-			return true;
-		}
-		return false;
-	}
-	
-	public static boolean wearingVoid(Player player) {
-		boolean hasHelm = false;
-		int correctEquipment = 0;
-		if (player.checkItem(Equipment.BODY_SLOT, eRobes[0]) || player.checkItem(Equipment.BODY_SLOT, normRobes[0])) {
-			correctEquipment++;
-		}
-		if (player.checkItem(Equipment.LEG_SLOT, eRobes[1]) || player.checkItem(Equipment.LEG_SLOT, normRobes[1])) {
-			correctEquipment++;
-		}
-		if (player.checkItem(Equipment.HEAD_SLOT, helms[0]) || player.checkItem(Equipment.HEAD_SLOT, helms[1]) || player.checkItem(Equipment.HEAD_SLOT, helms[2])) {
-			hasHelm = true;
-			correctEquipment++;
-		}
-		if (player.checkItem(Equipment.SHIELD_SLOT, deflector)) {
-			correctEquipment++;
-		}
-		if (player.checkItem(Equipment.HANDS_SLOT, gloves)) {
-			correctEquipment++;
-		}
-		if (player.checkItem(Equipment.WEAPON_SLOT, mace)) {
-			correctEquipment++;
-		}
-		
-		if (correctEquipment > 3 && hasHelm) {
-			//System.out.println("Returned true.");
-			return true;
-		} 
-		//System.out.println("Returned false.");
-		return false;
-	}
+	@JvmStatic
+	fun voidElite(player: Player): Boolean {
+        return wearingVoid(player) && player.checkItem(
+                Equipment.BODY_SLOT,
+                eRobes[0]
+            ) && player.checkItem(Equipment.LEG_SLOT, eRobes[1])
+    }
 
+    @JvmStatic
+	fun voidRange(player: Player): Boolean {
+        return wearingVoid(player) && player.checkItem(Equipment.HEAD_SLOT, helms[1])
+    }
+
+    @JvmStatic
+	fun voidMelee(player: Player): Boolean {
+        return wearingVoid(player) && player.checkItem(Equipment.HEAD_SLOT, helms[0])
+    }
+
+    @JvmStatic
+	fun voidMage(player: Player): Boolean {
+        return wearingVoid(player) && player.checkItem(Equipment.HEAD_SLOT, helms[2])
+    }
+
+    @JvmStatic
+	fun wearingVoid(player: Player): Boolean {
+        var hasHelm = false
+        var correctEquipment = 0
+        if (player.checkItem(Equipment.BODY_SLOT, eRobes[0]) || player.checkItem(Equipment.BODY_SLOT, normRobes[0])) {
+            correctEquipment++
+        }
+        if (player.checkItem(Equipment.LEG_SLOT, eRobes[1]) || player.checkItem(Equipment.LEG_SLOT, normRobes[1])) {
+            correctEquipment++
+        }
+        if (player.checkItem(Equipment.HEAD_SLOT, helms[0]) || player.checkItem(
+                Equipment.HEAD_SLOT,
+                helms[1]
+            ) || player.checkItem(Equipment.HEAD_SLOT, helms[2])
+        ) {
+            hasHelm = true
+            correctEquipment++
+        }
+        if (player.checkItem(Equipment.SHIELD_SLOT, deflector)) {
+            correctEquipment++
+        }
+        if (player.checkItem(Equipment.HANDS_SLOT, gloves)) {
+            correctEquipment++
+        }
+        if (player.checkItem(Equipment.WEAPON_SLOT, mace)) {
+            correctEquipment++
+        }
+        return correctEquipment > 3 && hasHelm
+        //System.out.println("Returned false.");
+    }
 }
