3c5479019b6d36a37c4a89fe4674bceae3f5e18c
Titas <titas@lacunaclientscope.com>
2022-11-21 22:24:08 +0000

    byteStream to kotlin

--- src/main/java/com/realting/world/clip/stream/ByteStream.kt
+++ src/main/java/com/realting/world/clip/stream/ByteStream.kt
@@ -1,108 +1,67 @@
-package com.realting.world.clip.stream;
+package com.realting.world.clip.stream
 
-public class ByteStream {
-
-	private byte[] buffer;
-	public int offset;
-
-	public ByteStream(byte[] buffer)
-	{
-		this.buffer = buffer;
-		this.offset = 0;
-	}
-
-	public void skip(int length)
-	{
-		offset += length;
-	}
-
-	public void setOffset(int position)
-	{
-		offset = position;
-	}
-	
-	public void setOffset(long position)
-	{
-		offset = (int) position;
-	}
-
-	public int length()
-	{
-		return buffer.length;
-	}
-	
-	public byte getByte()
-	{
-		return buffer[offset++];
-	}
-
-	public int getUByte()
-	{
-		return buffer[offset++] & 0xff;
-	}
-
-	public int getShort()
-	{
-		int val = (getByte() << 8) + getByte();
-		if(val > 32767)
-            		val -= 0x10000;
-		return val;
-	}
-
-	public int getUShort()
-	{
-        return (getUByte() << 8) + getUByte();
+class ByteStream(private val buffer: ByteArray) {
+    var offset = 0
+    fun skip(length: Int) {
+        offset += length
     }
-	
-	public int getInt()
-	{
-		return (getUByte() << 24) + (getUByte() << 16) + (getUByte() << 8) + getUByte();
-	}
-	
-	public long getLong()
-	{
-		return (getUByte() << 56) + (getUByte() << 48) + (getUByte() << 40) + (getUByte() << 32) + (getUByte() << 24) + (getUByte() << 16) + (getUByte() << 8) + getUByte();
-	}
 
-	public int getUSmart()
-	{
-        int i = buffer[offset] & 0xff;
-        if (i < 128) {
-        	return getUByte();
-        } else {
-        	return getUShort() - 32768;
+    fun setOffset(position: Long) {
+        offset = position.toInt()
+    }
+
+    fun length(): Int {
+        return buffer.size
+    }
+
+    val byte: Byte
+        get() = buffer[offset++]
+    val uByte: Int
+        get() = buffer[offset++].toInt() and 0xff
+    val short: Int
+        get() {
+            var `val`: Int = (byte.toInt()  shl 8) + byte
+            if (`val` > 32767) `val` -= 0x10000
+            return `val`
         }
-    }
-	
-	public int getMedium()
-	{
-		offset += 3;
-		return ((buffer[offset - 3] & 0xff) << 16) | ((buffer[offset - 2] & 0xff) << 8) | (buffer[offset - 1] & 0xff);
-	}
+    val uShort: Int
+        get() = (uByte shl 8) + uByte
+    val int: Int
+        get() = (uByte shl 24) + (uByte shl 16) + (uByte shl 8) + uByte
+    val long: Long
+        get() = ((uByte shl 56) + (uByte shl 48) + (uByte shl 40) + (uByte shl 32) + (uByte shl 24) + (uByte shl 16) + (uByte shl 8) + uByte).toLong()
+    val uSmart: Int
+        get() {
+            val i: Int = buffer[offset].toInt()  and 0xff
+            return if (i < 128) {
+                uByte
+            } else {
+                uShort - 32768
+            }
+        }
+    val medium: Int
+        get() {
+            offset += 3
+            return buffer[offset - 3].toInt()  and 0xff shl 16 or (buffer[offset - 2].toInt()  and 0xff shl 8) or (buffer[offset - 1].toInt()  and 0xff)
+        }
+    val nString: String
+        get() {
+            val i = offset
+            while (buffer[offset++].toInt() != 0);
+            return String(buffer, i, offset - i - 1)
+        }
+    val bytes: ByteArray
+        get() {
+            val i = offset
+            while (buffer[offset++].toInt() != 10);
+            val abyte0 = ByteArray(offset - i - 1)
+            System.arraycopy(buffer, i, abyte0, i - i, offset - 1 - i)
+            return abyte0
+        }
 
-	public String getNString()
-    {
-		int i = offset;
-		while(buffer[offset++] != 0) ;
-		return new String(buffer, i, offset - i - 1);
+    fun read(length: Int): ByteArray {
+        val b = ByteArray(length)
+        for (i in 0 until length) b[i] = buffer[offset++]
+        return b
     }
-
-	public byte[] getBytes()
-    {
-        int i = offset;
-        while(buffer[offset++] != 10) ;
-        byte abyte0[] = new byte[offset - i - 1];
-        System.arraycopy(buffer, i, abyte0, i - i, offset - 1 - i);
-        return abyte0;
-    }
-	
-	public byte[] read(int length)
-	{
-		byte[] b = new byte[length];
-		for (int i = 0; i < length; i++)
-			b[i] = buffer[offset++];
-		return b;
-	}
-
 }
-	
